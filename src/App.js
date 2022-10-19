2b325f9cc2e03eb5778fbee75de5253dc37572f8
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-19 21:23:13 +0200

    Replace src with 28-Stu-Mini-Project files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import Welcome from './components/Welcome';
+import './App.css';
+import BucketList from './components/BucketList';
 
 function App() {
-  // Here we declare a state boolean variable "loggedIn" and a function to update it.
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  // We return the Welcome component and pass loggedIn and setLoggedIn as props.
-  return <Welcome loggedIn={loggedIn} setLoggedIn={setLoggedIn} />;
+  return (
+    <div className="bucket-app">
+      <BucketList />
+    </div>
+  );
 }
 
 export default App;
